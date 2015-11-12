b8d2448e54c4b734b2e65288e0b31a2ac75d1588
agb91 <andrea.damio1991@hotmail.it>
2015-11-12 11:59:16 +0100

    little favicon disaster solved

--- test/controllers/altro_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AltroControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
--- test/controllers/second_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SecondControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
