f9b5b7ce468f9b5005abf42a853891844051aaf0
agb91 <andrea.damio1991@hotmail.it>
2015-11-12 15:07:59 +0100

    now work fast and with services

--- app/assets/javascripts/second.js
+++ app/assets/javascripts/second.js
@@ -1,12 +1,62 @@
-var app = angular.module('second', [])
-app.controller('index', ['$scope' , function($scope) {
+var app = angular.module('second',[])
 
-    $scope.primovet = [1,2,3,4,5,6];
-    $scope.secondovet = [7,8,9,10,11,12];
-    $scope.oggetti = [$scope.primovet, $scope.secondovet];
+app.controller("index", function($scope, splitter) {
 
-}]);
-app.directive('honeyComb', function() {
+    $scope.oggetti = [1,2,3,4,5,6,7,8,9,10,11,12];
+    $scope.oggetti = splitter.splitter($scope.oggetti,5);
+
+});
+
+app.service("splitter", function() {
+        this.splitter = function(v,c)
+        {
+          v = v.reverse();
+          var ris = []
+          var tot = v.length;
+          for (var i=0; i<tot; i=i+c)
+          {
+            nuovo = getter(v,c);
+            ris.push(nuovo)
+          }
+          return ris
+        };
+
+        this.half = function(v)
+        {
+          var ris1 = [];
+          var ris2 = [];
+          var risTot = []
+          var l = v.length;
+          for (var i = 0; i<l; i++)
+          {
+            if(i<(l/2))
+            {
+              ris1.push(v.pop());
+            }
+            else
+            {
+              ris2.push(v.pop());
+            }
+          }
+          risTot = [ris1,ris2];
+          return risTot;
+        };
+
+        var getter = function(v,c)
+        {
+          var r = []
+          for(var i=0; i<c; i++)
+          {
+            if(v.length>0)
+            {
+              r.push(v.pop());
+            }
+          }
+          return r;
+        }
+    });
+
+app.directive("honeyComb", function(splitter) {
     return {
         restrict: 'EAC',
         templateUrl: "/second/honeycomb.html",
@@ -21,28 +71,8 @@ app.directive('honeyComb', function() {
               return v;
           };
 
-          var half = function(v) {
-              var ris1 = [];
-              var ris2 = [];
-              var risTot = []
-              var l = v.length;
-              for (var i = 0; i<l; i++)
-              {
-                if(i<(l/2))
-                {
-                  ris1.push(v.pop());
-                }
-                else
-                {
-                  ris2.push(v.pop());
-                }
-              }
-              risTot = [ris1,ris2];
-              return risTot;
-          };
-
           scope.oggetti = formatVector(scope.name);
-          scope.comb = half(scope.oggetti);
+          scope.comb = splitter.half(scope.oggetti);
         }
     };
 });
